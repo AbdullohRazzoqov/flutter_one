d74e1320343e9588de6af2b5541e486f5d3681b7
Shohjahon <shohjahonrazzoqov@gmail.com>
2022-10-06 23:50:33 -0400

    expanded update

--- lib/expanded/example.dart
+++ lib/expanded/example.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 //Expanded => Farzadlarni mahsus tahsimlash uchun kerak bo'ladi 
 //children ichida bo'lishi shart
+//bizning widgetlarimiz soni oshgan sayin size oshgan sayin muammoga duch kelish 
+//boshlanadi shundan biri ekranga sig'may qolishi shuning oldini olish uchun 
+//expandeddan foylananishimiz mumkin u siqib beradi yoki teng tahsimlab oladi
 class ExpandedWidget extends StatelessWidget {
   const ExpandedWidget({super.key});
 
@@ -10,20 +13,18 @@ class ExpandedWidget extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        title: const Text("Column and Row"),
+        title: const Text("Expanded"),
       ),
       body: Container(
           color: Colors.yellowAccent.shade100,
-          // ? Row => SATR 
-          // ? Column => USTUN
           child: Row(
             mainAxisSize: MainAxisSize.min,
        
             children: const [
-              Icon(Icons.circle_notifications,color: Colors.amberAccent,size: 60,),
-              Icon(Icons.circle_notifications,color: Colors.indigo,size: 60,),
-              Icon(Icons.circle_notifications,color: Colors.indigoAccent,size: 60,),
-              Icon(Icons.circle_notifications,color: Colors.orange,size: 60,),
+              Expanded(child: Icon(Icons.circle_notifications,color: Colors.amberAccent,size: 60,)),
+              Expanded(child: Icon(Icons.circle_notifications,color: Colors.indigo,size: 60,)),
+              Expanded(child: Icon(Icons.circle_notifications,color: Colors.indigoAccent,size: 60,)),
+              Expanded(child: Icon(Icons.circle_notifications,color: Colors.orange,size: 60,)),
             
          
             ],

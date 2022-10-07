387a72deefd8fdfd7c76860a9239d6595d40ef48
Shohjahon <shohjahonrazzoqov@gmail.com>
2022-10-07 00:12:31 -0400

    expanded update

--- lib/expanded/example.dart
+++ lib/expanded/example.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-//Expanded => Farzadlarni mahsus tahsimlash uchun kerak bo'ladi 
+
+//Expanded => Farzadlarni mahsus tahsimlash uchun kerak bo'ladi
 //children ichida bo'lishi shart
-//bizning widgetlarimiz soni oshgan sayin size oshgan sayin muammoga duch kelish 
-//boshlanadi shundan biri ekranga sig'may qolishi shuning oldini olish uchun 
+//bizning widgetlarimiz soni oshgan sayin size oshgan sayin muammoga duch kelish
+//boshlanadi shundan biri ekranga sig'may qolishi shuning oldini olish uchun
 //expandeddan foylananishimiz mumkin u siqib beradi yoki teng tahsimlab oladi
 class ExpandedWidget extends StatelessWidget {
   const ExpandedWidget({super.key});
@@ -19,16 +20,34 @@ class ExpandedWidget extends StatelessWidget {
           color: Colors.yellowAccent.shade100,
           child: Row(
             mainAxisSize: MainAxisSize.min,
-       
             children: const [
-              Expanded(child: Icon(Icons.circle_notifications,color: Colors.amberAccent,size: 60,)),
-              Expanded(child: Icon(Icons.circle_notifications,color: Colors.indigo,size: 60,)),
-              Expanded(child: Icon(Icons.circle_notifications,color: Colors.indigoAccent,size: 60,)),
-              Expanded(child: Icon(Icons.circle_notifications,color: Colors.orange,size: 60,)),
-            
-         
+              Expanded(
+                flex: 20,
+                child: Icon(
+                  Icons.circle_notifications,
+                  color: Colors.amberAccent,
+                  size: 60,
+                ),
+              ),
+              Expanded(
+                flex: 20,
+                child: Icon(
+                  Icons.circle_notifications,
+                  color: Colors.indigo,
+                  size: 60,
+                ),
+              ),
+              Expanded(
+                flex: 60,
+                child: Icon(
+                  Icons.circle_notifications,
+                  color: Colors.indigoAccent,
+                  size: 60,
+                ),
+              ),
             ],
           )),
-    );;
+    );
+    ;
   }
 }
